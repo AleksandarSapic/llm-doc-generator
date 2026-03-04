dc82e503c98e460e1415dd416d567c3fee6f3351
Aleksandar Sapic <sapicalleksandar@gmail.com>
2026-03-04 18:47:32 +0100

    initial commit

---
+++ backend/src/main/java/com/aleksandarsapic/llm_doc_generator/LlmDocumentationGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.aleksandarsapic.llm_doc_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmDocumentationGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmDocumentationGeneratorApplication.class, args);
+	}
+
+}
